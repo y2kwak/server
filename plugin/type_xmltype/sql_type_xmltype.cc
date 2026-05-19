6b79d98fc000960f3d64f3ad7791fefbaa102c1d
Alexey Botchkov <holyfoot@askmonty.org>
2026-05-19 15:27:22 +0400

    MDEV-39653 AddressSanitizer: heap-use-after-free in my_mb_wc_latin1/../Field_xmltype::store after invalid xml.
    The Field_blob::store can affect the 'from' argument so it can't be used
    after it.

--- plugin/type_xmltype/sql_type_xmltype.cc
+++ plugin/type_xmltype/sql_type_xmltype.cc
@@ -216,13 +216,13 @@ int Field_xmltype::store(const char *from, size_t length, CHARSET_INFO *cs)
   return Field_blob::store(from, length, cs);
 
 err:
+  my_error(ER_WRONG_VALUE, MYF(0),
+           "XMLTYPE", ErrConvString(from, length, cs).ptr());
+
   if (maybe_null())
     set_null();
   else
     Field_blob::store(STRING_WITH_LEN("<invalid_xml_replaced />"), cs);
-
-  my_error(ER_WRONG_VALUE, MYF(0),
-           "XMLTYPE", ErrConvString(from, length, cs).ptr());
   return -1;
 }
 
